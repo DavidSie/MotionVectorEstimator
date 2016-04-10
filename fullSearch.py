2f2f7cfe5eaf778e82c3ae60e49c2229847dfcfe
David Siecinski <david.siecinski@impaq.com.pl>
2016-04-10 11:31:56 +0200

    Add new class fullSearch with tests, both under construction

--- fullSearch.py
+++ fullSearch.py
@@ -1,2 +1,85 @@
 __author__ = 'davidsiecinski'
 
+import sys
+class FullSearch:
+    def __init__(self,current_picture,referenced_picture,n=2,p=2):
+        # 2d lists
+        self.current_picture=current_picture
+        self.referenced_picture=referenced_picture
+        self.n=n # size of macroblock
+        self.p=p # defines searched area [-p,p]
+        self.x=0 #curent coordinate of top left corner of macroblock
+        self.y=0 #curent coordinate of top left corner of macroblock
+
+    def setCurrentPicture(self,current_picture):
+        self.picture=current_picture
+
+    def setReferencedPicture(self,referenced_picture):
+        self.referenced_picture=referenced_picture
+
+    def __sumOfAbsoluteDifferences__(self,n,m):
+        sum=0.0
+        for i in range(self.n):
+            for j in range(self.n):
+                try:
+                    sum = sum + (self.__makroBlock__(i,j)-self.__makroBlock__(i+n,j+m,isCurrent=False))*(self.__makroBlock__(i,j)-self.__makroBlock__(i+n,j+m,isCurrent=False))
+                except IndexError:
+                    print "IndexError for n,m=[",n, ",", m, "] i=", i, " j=", j," sum=",sum
+                    continue
+                # print " n,m=[",n, ",", m, "] i=", i, " j=", j," sum=",sum
+        # print 'n=',n,' m=',m,' sum=',sum
+        return sum
+
+    # macroblock is defined by top left corner and size self.N
+    # returns a cut part of picture
+    def __makroBlock__(self,i,j,isCurrent=True):
+        y=self.y+i
+        x=self.x+j
+        if x>=len(self.current_picture[0]) or x<0: # python allows negative index but I dont
+             raise IndexError('x out of list')
+        if  y>=len(self.current_picture) or  y<0:
+             raise IndexError('y out of list')
+        if isCurrent:
+            # print "ref_pict[",y,"]","[",x,"]= ",list(reversed(self.current_picture))[y][x]
+            # print "row",list(reversed(self.current_picture))[y]
+            return list(reversed(self.current_picture))[y][x]
+        else:
+            # print "ref_pict[",y,"]","[",x,"]= ",list(reversed(self.referenced_picture))[y][x]
+            # print "row",list(reversed(self.referenced_picture))[y]
+            return list(reversed(self.referenced_picture))[y][x]
+
+    # returns movment of 1 macro block
+    # returned Motion vector has minimal SAD.
+    def motionVector(self):
+        p_range=range(-self.p,self.p)
+        min_n=None
+        min_m=None
+        min_value=sys.float_info.max
+
+        for n in p_range:
+            for m in p_range:
+                value=self.__sumOfAbsoluteDifferences__(n,m)
+                if value<min_value:
+                    min_value=value
+                    min_n=n
+                    min_m=m
+        return [min_n,min_m]
+
+    #   returns matrix of Motion vectors
+    def motionEstimation(self):
+        num_of_macroblocs_in_y=len(self.current_picture)/self.n
+        num_of_macroblocs_in_x=len(self.current_picture[0])/self.n
+        result=[]
+        for y in range(num_of_macroblocs_in_y):
+            row=[]
+            for x in range(num_of_macroblocs_in_x):
+                self.x=x*self.n
+                self.y=y*self.n
+                print 'x=',self.x,' y=',self.y
+                row.append(self.motionVector())
+            result.append(row)
+        return result
+
+
+
+    # dla wybranych
--- unittest/fullSearch_tests.py
+++ unittest/fullSearch_tests.py
@@ -1,2 +1,47 @@
 __author__ = 'davidsiecinski'
+import unittest
+import fullSearch
 
+class PreProcessingMethods (unittest.TestCase):
+    def setUp(self):
+        current_picture=[[0,0,0,0],[0,1,0,0],[0,3,0,0],[0,0,0,0]]
+        reference_picture=[[0,0,0,0],[0,0,1,0],[0,0,3,0],[0,0,0,0]]
+        self.fullsearch_= fullSearch.FullSearch(current_picture,reference_picture)
+
+        small_current_picture=[[0,1,0,0]]
+        small_reference_picture=[[0,0,1,0]]
+        self.small_fullsearch = fullSearch.FullSearch(small_current_picture,small_reference_picture)
+
+    def test_macroblock(self):
+        # y is reverse counted  to fit x and y axis
+        self.fullsearch_.y=2
+        self.fullsearch_.x=0
+        i=0
+        j=1
+        self.assertEqual(self.fullsearch_.__makroBlock__(i,j),1)
+
+    def test_macroblock2(self):
+        self.fullsearch_.y=2
+        self.fullsearch_.x=1
+        i=0
+        j=1
+        self.assertEqual(self.fullsearch_.__makroBlock__(i,j,isCurrent=False),1)
+
+    def test_motionVector(self):
+        self.small_fullsearch.p = 2
+        return_value = [0, 1]
+        # coordinates of top left corner of macro block
+        self.small_fullsearch. y= 0
+        self.small_fullsearch.x = 0
+        self.assertEqual(self.small_fullsearch.motionVector(), return_value)
+
+    def test_motionEstimation(self):
+        # result should fit one of 2 choices
+        result1=[[[0, 1],[0, -1]], [[0, -1],[0, 1]]]
+        result2=[[[0, 1],[-1, -1]], [[1, -1],[0, 1]]]
+
+        self.assertEqual(self.fullsearch_.motionEstimation(),result1)
+
+
+    # def test_url_preprocessing(self):
+    #     self.assertEqual(self.preprocessing_.preprocess_url("https://www.google.pl"), 'URL')
