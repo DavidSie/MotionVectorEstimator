b3db3ce74c2ed0db166def7e1e1a09ddcb21aa26
krzy288 <krzysztof.olejniczak288@gmail.com>
2016-05-21 04:19:18 +0200

    Logsearch-main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import  imageReader
 import fullSearch
 import logsearch
-# import diamondSearch
+#import diamondSearch
 
 def raw_input_with_default(text,default):
     input = raw_input(text+'['+default +']'+ chr(8)*4)
@@ -20,15 +20,18 @@ referenced_picture=[]
 
 
 print "Welcome to ",__name__," ",__version__
-feed_in=raw_input("Choose: Full, Log or Diamond Search")
+
 current_picture_path=raw_input_with_default("Path to first(current) picture",'1.jpg')
 referenced_picture_path=raw_input_with_default("Path to second(referenced) picture",'2.jpg')
+feed_in=raw_input("Choose: Full, Log or Diamond Search")
 
 
 current_picture=imageReader.loadImage(current_picture_path)
 referenced_picture=imageReader.loadImage(referenced_picture_path)
 p=len(current_picture)/20 # half of search window
 n=5 # size of macroblock
+
+
 
 if "full" in feed_in.lower() :
     full_=fullSearch.FullSearch(current_picture=current_picture,referenced_picture=referenced_picture,n=n,p=p)
@@ -38,6 +41,12 @@ if "diamond" in feed_in.lower() :
     exit()
 if "log" in feed_in.lower() :
     # TODO fill me with log search code
+    log = logsearch.LogSearch(current_picture=current_picture,referenced_picture=referenced_picture,n=n,p=p )
+    print log.motionVector()
+    #exit()
+else:
+    print "Not found"
     exit()
+
 
 
