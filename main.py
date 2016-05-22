cf4563fd869bfa0cdf13a72a5063daeb9034b234
David Siecinski <david.siecinski@impaq.com.pl>
2016-05-22 17:33:42 +0200

    time measuring and calculating whole motion estimation

--- main.py
+++ main.py
@@ -2,17 +2,17 @@
 import  imageReader
 import fullSearch
 import logsearch
+import time
 #import diamondSearch
+
+__name__='MotionVectorEstimator'
+__version__='1.0.0rc1'
 
 def raw_input_with_default(text,default):
     input = raw_input(text+'['+default +']'+ chr(8)*4)
     if not input:
         input = default
     return input
-
-__name__='MotionVectorEstimator'
-__version__='1.0.0rc1'
-
 
 current_picture=[]
 referenced_picture=[]
@@ -28,25 +28,28 @@ feed_in=raw_input("Choose: Full, Log or Diamond Search")
 
 current_picture=imageReader.loadImage(current_picture_path)
 referenced_picture=imageReader.loadImage(referenced_picture_path)
-p=len(current_picture)/20 # half of search window
-n=5 # size of macroblock
+p= 4# len(current_picture)/70 # half of search window
+n=4 # size of macroblock
 
-
-
+motion_estimation=None
+start = time.time()
 if "full" in feed_in.lower() :
-    full_=fullSearch.FullSearch(current_picture=current_picture,referenced_picture=referenced_picture,n=n,p=p)
-    print full_.motionVector()
-if "diamond" in feed_in.lower() :
+    full_ = fullSearch.FullSearch(current_picture=current_picture,referenced_picture=referenced_picture,n=n,p=p)
+    motion_estimation = full_.motionEstimation()
+    print motion_estimation
+elif "diamond" in feed_in.lower() :
     # TODO fill me with diamond search code
+    print motion_estimation
     exit()
-if "log" in feed_in.lower() :
-    # TODO fill me with log search code
+elif "log" in feed_in.lower() :
     log = logsearch.LogSearch(current_picture=current_picture,referenced_picture=referenced_picture,n=n,p=p )
-    print log.motionVector()
-    #exit()
+    motion_estimation = log.motionEstimation()
+    print motion_estimation
 else:
     print "Not found"
     exit()
-
+end = time.time()
+running_time=(end - start)
+print "it took: ",running_time, "s"
 
 
